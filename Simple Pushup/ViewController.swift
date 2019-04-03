681f7fd3084d21dbfefa892dfc863e00e6be4002
Johan Park <parkjohan@gmail.com>
2019-04-03 16:36:14 -0700

    Initial Commit

---
+++ Simple Pushup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Pushup
+//
+//  Created by Johan Park on 4/3/19.
+//  Copyright © 2019 Johan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Simple PushupTests/Simple_PushupTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_PushupTests.swift
+//  Simple PushupTests
+//
+//  Created by Johan Park on 4/3/19.
+//  Copyright © 2019 Johan Park. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Pushup
+
+class Simple_PushupTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
